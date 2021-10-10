dd56dd1d13c400951306f86285c9a364b4244e60
FMOLDOVAN <franciscmoldovan93@gmail.com>
2021-10-10 21:31:07 +0300

    Serialized models to join in retrieved data

--- scrumboard/serializers.py
+++ scrumboard/serializers.py
@@ -2,13 +2,16 @@ from rest_framework import serializers
 from scrumboard.models import List, Card
 
 
-class ListSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = List
-        fields = '__all__'
-
-
 class CardSerializer(serializers.ModelSerializer):
     class Meta:
         model = Card
+        fields = '__all__'
+
+
+class ListSerializer(serializers.ModelSerializer):
+    # line below lists scrum cards in list directly
+    cards = CardSerializer(read_only=True, many=True)
+
+    class Meta:
+        model = List
         fields = '__all__'
