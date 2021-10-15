7e17936e0559388c34609cccedae2b32f9c87c2a
Francisc Moldovan <fmoldovan@jouve.com>
2021-10-15 18:40:57 +0300

    Added authentication condition on ListViewSet

--- scrumboard/api.py
+++ scrumboard/api.py
@@ -1,6 +1,7 @@
 from rest_framework.viewsets import ModelViewSet
 from scrumboard.serializers import ListSerializer, CardSerializer
 from scrumboard.models import List, Card
+from rest_framework import permissions
 
 """
 ModelViewSet allows GET PUT POST DELETE
@@ -11,9 +12,11 @@ in older commits it was smth else
 class ListViewSet(ModelViewSet):
     queryset = List.objects.all()
     serializer_class = ListSerializer
+    permission_classes = (permissions.IsAuthenticated, )
 
 
 class CardViewSet(ModelViewSet):
     queryset = Card.objects.all()
     serializer_class = CardSerializer
+    permission_classes = (permissions.IsAuthenticated,)
 
